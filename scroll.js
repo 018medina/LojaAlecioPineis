37e46b8b9162b8064bc0db2f04e5d974164f6e77
FABIO MEDINA <018.medina@gmail.com>
2025-02-13 21:37:49 -0300

    atualizações site

--- scroll.js
+++ scroll.js
@@ -1,23 +1,32 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     ScrollReveal({ reset: true });
 
     animaDireita();
     animaEsquerda();
+    animaAparecer();
 });
 
-function animaDireita(){
+function animaDireita() {
     ScrollReveal().reveal('.animaDireita', {
         origin: 'right',
         duration: 2000,
-        distance: '20%'
+        distance: '100px'
     })
 }
 
-function animaEsquerda(){
+function animaEsquerda() {
     ScrollReveal().reveal('.animaEsquerda', {
         origin: 'left',
         duration: 2000,
-        distance: '20%'
+        distance: '100px'
+    })
+}
+
+function animaAparecer() {
+    ScrollReveal().reveal('.animaAparecer', {
+        duration: 1000, // Tempo da animação
+        opacity: 0, // Garante transição de opacidade
+        easing: 'ease-in-out' // Suaviza a transição
     })
 }
